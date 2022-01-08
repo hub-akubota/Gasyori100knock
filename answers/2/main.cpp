786c1d1f7edda85c6cfd60720b60c5cfa77f37d3
Arisa Kubota <kubotaarisa@team-lab.com>
2022-01-08 19:30:08 +0900

    indentを修正

--- answers/2/main.cpp
+++ answers/2/main.cpp
@@ -5,7 +5,7 @@
 
 void bgr2gray(const cv::Mat &src, cv::Mat &dst) {
     std::vector<cv::Mat> planes;
-	cv::split(src, planes);
+    cv::split(src, planes);
     dst = planes[0] * 0.0722 + planes[1] * 0.7152 + planes[2] * 0.2126;
     cv::threshold(dst, dst, 0, 255, cv::THRESH_TOZERO);
     cv::threshold(dst, dst, 255, 255, cv::THRESH_TRUNC);
