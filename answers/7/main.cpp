6318963fa34f163b8e968851438d98715f9efa20
Arisa Kubota <kubotaarisa@team-lab.com>
2022-01-08 21:56:20 +0900

    tmp不要だから削除

--- answers/7/main.cpp
+++ answers/7/main.cpp
@@ -5,9 +5,8 @@
 #include <unistd.h>
 
 void pool_average(const cv::Mat &src, cv::Mat &dst, int ksize_h = 8, int ksize_w = 8) {
-    cv::Mat tmp = src.clone();
     dst = src.clone();
-    tmp.convertTo(tmp, CV_32FC3);
+    dst.convertTo(dst, CV_32FC3);
 
     int h = src.size().height;
     int w = src.size().width;
@@ -30,10 +29,10 @@ void pool_average(const cv::Mat &src, cv::Mat &dst, int ksize_h = 8, int ksize_w
                     if (col >= w) col = w - 1;
                     if (row < 0) row = 0;
                     if (row >= h) row = h - 1;
-                    value_tot += tmp.at<cv::Vec3f>(row, col);
+                    value_tot += dst.at<cv::Vec3f>(row, col);
                 }
             }
-            cv::Vec3b mean = value_tot / float(ksize_w * ksize_h);
+            cv::Vec3f mean = value_tot / float(ksize_w * ksize_h);
             for (int step_w=0; step_w<ksize_w; step_w++) {
                 for (int step_h=0; step_h<ksize_h; step_h++) {
                     int col = ksize_w * x - line_left + step_w;
@@ -42,12 +41,12 @@ void pool_average(const cv::Mat &src, cv::Mat &dst, int ksize_h = 8, int ksize_w
                     if (col >= w) continue;
                     if (row < 0) continue;
                     if (row >= h) continue;
-                    dst.at<cv::Vec3b>(row, col) = mean;
+                    dst.at<cv::Vec3f>(row, col) = mean;
                 }
             }
         }
     }
-    std::cout << "Done" << std::endl;
+    dst.convertTo(dst, CV_8UC3);
 }
 
 int main() {
